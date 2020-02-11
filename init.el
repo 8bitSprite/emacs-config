fc649d596de2372ffc2fa041f18b10fd452e334c
Mike Holsather <mholsather@gmail.com>
2020-02-11 10:30:36 -0600

    initial commit

---
+++ init.el
@@ -0,0 +1,429 @@
+;; ===========================
+;; Package system and Sources
+;; ===========================
+
+(require 'package)
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                 (not (gnutls-available-p))))
+    (proto (if no-ssl "http" "https")))
+    ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
+    (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+    ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+    (when (< emacs-major-version 24)
+    ;; For important compatibility libraries like cl-lib
+    (add-to-list 'package-archives '("gnu" . (concat proto "://elpa.gnu.org/packages/")))))
+
+(package-initialize)
+
+;; We will use 'use-package' to install and configure packages.
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(eval-when-compile (require 'use-package))
+
+;; No need to out 'ensure' everywhere, since we don't use anything else to install packages.
+(setq use-package-always-ensure t)
+
+;; Pass system shell environment to Emacs. This is important primarily for shell inside Emacs, but also things like Org mode export to Tex PDF don't work, since it relies on running external command pdflatex, which is loaded from PATH.
+(use-package exec-path-from-shell
+  :ensure t)
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize))
+
+;; ===============
+;; Sane Defaults
+;; ===============
+
+;; Don't bother with auto save and backups.
+(setq auto-save-default nil)
+(setq make-backup-files nil)
+
+;; Warn only when opening files bigger than 100MB
+(setq large-file-warning-threshold 100000000)
+
+;; Move file to trash instead of removing.
+(setq-default delete-by-moving-to-trash t)
+
+;; Revert (update) buffers automatically when underlying files are changed externally.
+(global-auto-revert-mode t)
+
+;; Global line numbers
+(add-hook 'prog-mode-hook 'linum-mode)
+
+(add-hook 'org-mode-hook 'nolinum)
+
+(setq
+ inhibit-startup-message t         ; Don't show the startup message...
+ inhibit-startup-screen t          ; ... or screen
+ cursor-in-non-selected-windows t  ; Hide the cursor in inactive windows
+ echo-keystrokes 0.1               ; Show keystrokes right away, don't show the message in the scratch buffer
+ initial-scratch-message nil       ; Empty scratch buffer
+ ring-bell-function 'ignore
+ sentence-end-double-space nil     ; Sentences should end in one space, come on!
+ confirm-kill-emacs 'y-or-n-p      ; y and n instead of yes and no when quitting
+ help-window-select t              ; Select help window so it's easy to quit it with 'q'
+)
+
+(fset 'yes-or-no-p 'y-or-n-p)      ; y and n instead of yes and no everywhere else
+(delete-selection-mode 1)          ; Delete selected text when typing
+(global-unset-key (kbd "s-p"))     ; Don't print
+
+;; Delete trailing spaces and add new line in the end of a file on save.
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+(setq require-final-newline t)
+
+;; =========
+;; WINDOWS
+;; =========
+
+(use-package windmove
+  ;; :defer 4
+  :ensure t
+  :config
+  ;; use command key on Mac
+  (windmove-default-keybindings 'super)
+  ;; wrap around at edges
+  (setq windmove-wrap-around t))
+
+;; =========
+;; VISUAL
+;; =========
+
+;; Font
+(when (window-system)
+  (set-frame-font "Fira Code"))
+(let ((alist '((33 . ".\\(?:\\(?:==\\|!!\\)\\|[!=]\\)")
+               (35 . ".\\(?:###\\|##\\|_(\\|[#(?[_{]\\)")
+               (36 . ".\\(?:>\\)")
+               (37 . ".\\(?:\\(?:%%\\)\\|%\\)")
+               (38 . ".\\(?:\\(?:&&\\)\\|&\\)")
+               (42 . ".\\(?:\\(?:\\*\\*/\\)\\|\\(?:\\*[*/]\\)\\|[*/>]\\)")
+               (43 . ".\\(?:\\(?:\\+\\+\\)\\|[+>]\\)")
+               (45 . ".\\(?:\\(?:-[>-]\\|<<\\|>>\\)\\|[<>}~-]\\)")
+               (46 . ".\\(?:\\(?:\\.[.<]\\)\\|[.=-]\\)")
+               (47 . ".\\(?:\\(?:\\*\\*\\|//\\|==\\)\\|[*/=>]\\)")
+               (48 . ".\\(?:x[a-zA-Z]\\)")
+               (58 . ".\\(?:::\\|[:=]\\)")
+               (59 . ".\\(?:;;\\|;\\)")
+               (60 . ".\\(?:\\(?:!--\\)\\|\\(?:~~\\|->\\|\\$>\\|\\*>\\|\\+>\\|--\\|<[<=-]\\|=[<=>]\\||>\\)\\|[*$+~/<=>|-]\\)")
+               (61 . ".\\(?:\\(?:/=\\|:=\\|<<\\|=[=>]\\|>>\\)\\|[<=>~]\\)")
+               (62 . ".\\(?:\\(?:=>\\|>[=>-]\\)\\|[=>-]\\)")
+               (63 . ".\\(?:\\(\\?\\?\\)\\|[:=?]\\)")
+               (91 . ".\\(?:]\\)")
+               (92 . ".\\(?:\\(?:\\\\\\\\\\)\\|\\\\\\)")
+               (94 . ".\\(?:=\\)")
+               (119 . ".\\(?:ww\\)")
+               (123 . ".\\(?:-\\)")
+               (124 . ".\\(?:\\(?:|[=|]\\)\\|[=>|]\\)")
+               (126 . ".\\(?:~>\\|~~\\|[>=@~-]\\)")
+               )
+             ))
+  (dolist (char-regexp alist)
+    (set-char-table-range composition-function-table (car char-regexp)
+                          `([,(cdr char-regexp) 0 font-shape-gstring]))))
+
+(use-package leuven-theme
+  :config
+  (load-theme 'leuven t)
+  (setq
+   leuven-scale-outline-headlines nil
+   leuven-scale-org-agenda-structure nil))
+
+;; Pretty icons
+(use-package all-the-icons)
+;; MUST DO M-x all-the-icons-install-fonts after
+
+;; Hide toolbar and scroll bar
+(tool-bar-mode -1)
+;; (scroll-bar-mode -1)
+
+;; Always wrap lines
+(global-visual-line-mode 1)
+
+;; Highlight current line
+(global-hl-line-mode 1)
+
+;; Show parens and other pairs.
+(use-package smartparens
+  :diminish
+  :config
+  (require 'smartparens-config)
+  (smartparens-global-mode t)
+  (show-smartparens-global-mode t))
+
+;; Spacemacs Powerline
+(use-package spaceline
+  :demand t
+  :init
+  (setq powerline-default-separator 'arrow-fade)
+  :config
+  (require 'spaceline-config)
+  (spaceline-spacemacs-theme))
+
+;; NeoTree
+(use-package neotree
+  :config
+  (setq neo-window-width 32
+        neo-create-file-auto-open t
+        neo-banner-message nil
+        neo-show-updir-line t
+        neo-window-fixed-size nil
+        neo-vc-integration nil
+        neo-mode-line-type 'neotree
+        neo-smart-open t
+        neo-show-hidden-files t
+        neo-mode-line-type 'none
+        neo-auto-indent-point t)
+  (setq neo-theme (if (display-graphic-p) 'nerd 'arrow))
+  (setq neo-hidden-regexp-list '("venv" "\\.pyc$" "~$" "\\.git" "__pycache__" ".DS_Store"))
+  (global-set-key (kbd "s-B") 'neotree-toggle))           ;; Cmd+Shift+b toggle treeX1
+
+;; Show full path in the title bar.
+(setq-default frame-title-format "%b (%f)")
+
+;; Never use tabs, use spaces instead.
+(setq tab-width 2)
+(setq js-indent-level 2)
+(setq css-indent-offset 2)
+(setq c-basic-offset 2)
+(setq-default indent-tabs-mode nil)
+(setq-default c-basic-offset 2)
+(setq-default tab-width 2)
+(setq-default c-basic-indent 2)
+
+;; Show keybindings cheatsheet
+(use-package which-key
+  :config
+  (which-key-mode)
+  (setq which-key-idle-delay 0.5))
+
+;; Disable blinking cursor.
+(blink-cursor-mode 0)
+
+;; ===============
+;; TEXT EDITING
+;; ===============
+
+;; Expand-region allows to gradually expand selection inside words, sentences, expressions, etc.
+(use-package expand-region
+  :config
+  (global-set-key (kbd "s-'") 'er/expand-Cmd)         ;; region+' (apostrophe) to expand
+  (global-set-key (kbd "s-\"") 'er/contract-region))     ;; Cmd+" (same, but with shift) to contract
+
+;; Move-text lines around with meta-up/down.
+(use-package move-text
+  :config
+  (move-text-default-bindings))
+
+;; Quickly insert new lines above or below the current line, with correct indentation.
+(defun smart-open-line ()
+  "Insert an empty line after the current line. Position the cursor at its beginning, according to the current mode."
+  (interactive)
+  (move-end-of-line nil)
+  (newline-and-indent))
+
+(defun smart-open-line-above ()
+  "Insert an empty line above the current line. Position the cursor at it's beginning, according to the current mode."
+  (interactive)
+  (move-beginning-of-line nil)
+  (newline-and-indent)
+  n(forward-line -1)
+  (indent-according-to-mode))
+
+(global-set-key (kbd "s-<return>") 'smart-open-line)            ;; Cmd+Return new line below
+(global-set-key (kbd "s-S-<return>") 'smart-open-line-above)    ;; Cmd+Shift+Return new line above
+
+;; Upcase and lowercase word or region, if selected.
+;; To capitalize or un-capitalize word use Alt+c and Alt+l
+(global-set-key (kbd "M-u") 'upcase-dwim)   ;; Alt+u upcase
+(global-set-key (kbd "M-l") 'downcase-dwim) ;; Alt-l lowercase
+
+;; Comment line or region.
+(global-set-key (kbd "s-/") 'comment-line)
+
+;; Visually find and replace text
+(use-package visual-regexp
+  :config
+  (define-key global-map (kbd "M-s-f") 'vr/replace)
+  (define-key global-map (kbd "s-r") 'vr/replace))  ;; Cmd+r find and replace
+
+
+;; =====================
+;; PROJECT MANAGEMENT
+;; =====================
+
+;; Use Projectile for project management.
+(use-package projectile
+  :config
+  (define-key projectile-mode-map (kbd "C-s-p") 'projectile-command-map) ;; Ctrl+Cmd+p show projectile menu
+  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+  (projectile-mode +1)
+  )
+
+
+;; =============================================
+;; MENUS AND COMPLETION (not code completion)
+;; =============================================
+
+;; Use minimalist Ivy for most things.
+(use-package ivy
+  :diminish                             ;; don't show Ivy in minor mode list
+  :config
+  (ivy-mode 1)                          ;; enable Ivy everywhere
+  (setq ivy-use-virtual-buffers t)      ;; show bookmarks and recent files in buffer list
+  (setq ivy-count-format "(%d/%d) ")
+  (setq ivy-display-style 'fancy)
+  (setq enable-recursive-minibuffers t)
+
+  (setq ivy-re-builders-alist
+      '((swiper . ivy--regex-plus)
+        (t      . ivy--regex-fuzzy)))   ;; enable fuzzy searching everywhere except for Swiper
+
+  (global-set-key (kbd "s-b") 'ivy-switch-buffer)  ;; Cmd+b show buffers and recent files
+  (global-set-key (kbd "M-s-b") 'ivy-resume))      ;; Alt+Cmd+b resume whatever Ivy was doing
+
+;; Swiper is a better local finder.
+(use-package swiper
+  :config
+  (global-set-key "\C-s" 'swiper)       ;; Default Emacs Isearch forward...
+  (global-set-key "\C-r" 'swiper)       ;; ... and Isearch backward replaced with Swiper
+  (global-set-key (kbd "s-f") 'swiper)) ;; Cmd+f find text
+
+;; Better menus with Counsel (a layer on top of Ivy)
+(use-package counsel
+  :config
+  (global-set-key (kbd "M-x") 'counsel-M-x)            ;; Alt+x run command
+  (global-set-key (kbd "s-P") 'counsel-M-x)            ;; Cmd+Shift+p run command
+  (global-set-key (kbd "C-x C-f") 'counsel-find-file)  ;; Replace built-in Emacs 'find file' (open file) with Counsel
+  (global-set-key (kbd "s-o") 'counsel-find-file))     ;; Cmd+o open file
+
+(use-package smex)  ;; show recent commands when invoking Alt-x (or Cmd+Shift+p)
+(use-package flx)   ;; enable fuzzy matching
+(use-package avy  ;; enable avy for quick navigation
+  :config
+  (global-set-key (kbd "C-:") 'avy-goto-char))
+
+;; Integrate Projectile with Counsel
+(use-package counsel-projectile
+  :config
+  (counsel-projectile-mode 1)
+  (global-set-key (kbd "C-x C-f") 'counsel-find-file)
+  (global-set-key (kbd "s-p") 'counsel-projectile-find-file)         ;; Cmd+p open file in current project
+  (global-set-key (kbd "s-F") 'counsel-projectile-ag))     ;; Cmd+Shift+F search in current git repository
+
+(setq projectile-completion-system 'ivy)             ;; Use Ivy in Projectile
+
+
+;; ===========================
+;; VERSION CONTROL WITH GIT
+;; ===========================
+
+;; Magit
+(use-package magit
+  :config
+  (global-set-key (kbd "s-g") 'magit-status))   ;; Cmd+g for git status
+
+;; Show changes in the gutter
+(use-package git-gutter
+  :diminish
+  :config
+  (global-git-gutter-mode 't)
+  (set-face-background 'git-gutter:modified 'nil)   ;; background color
+  (set-face-foreground 'git-gutter:added "green4")
+  (set-face-foreground 'git-gutter:deleted "red"))
+
+
+;; ==================
+;; CODE COMPLETION
+;; ==================
+
+(use-package company
+  :config
+  (setq company-idle-delay 0.1)
+  (setq company-global-modes '(not org-mode))
+  (setq company-minimum-prefix-length 1)
+  (add-hook 'after-init-hook 'global-company-mode))
+
+;; Set the company completion vocabulary to css and html when in web-mode.
+(defun my-web-mode-hook ()
+  (set (make-local-variable 'company-backends) '(company-css company-web-html company-yasnippet company-files)))
+
+;; ============
+;; PROGRAMMING
+;; ============
+
+(use-package yaml-mode)
+(use-package haml-mode)
+(use-package markdown-mode)
+(use-package yasnippet)
+(use-package web-mode
+  :config
+  (setq web-mode-markup-indent-offset 2)
+  (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.js?\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.css?\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.xml?\\'" . web-mode)))
+
+;; Emmet
+(use-package emmet-mode
+  :commands emmet-mode
+  :init
+  (setq emmet-indentation 2)
+  (setq emmet-move-cursor-between-quotes t)
+  :config
+  (add-hook 'sgml-mode-hook 'emmet-mode) ;; Auto-start on any markup modes
+  (add-hook 'css-mode-hook  'emmet-mode)) ;; enable Emmet's css abbreviation.
+;; Ctrl+j or Ctrl+Enter to expand
+
+(use-package lsp-mode
+  :commands lsp)
+
+;; for completions
+(use-package company-lsp
+  :after lsp-mode
+  :config (push 'company-lsp company-backends))
+
+(use-package vue-mode
+  :mode "\\.vue\\'"
+  :config
+  (add-hook 'vue-mode-hook #'lsp))
+
+;; ============
+;; Fun Stuff
+;; ============
+
+;; Weather
+;; weather from wttr.in
+(use-package wttrin
+  :ensure t
+  :commands (wttrin)
+  :init
+  (setq wttrin-default-cities '("Minnetonka"
+                                "Minneapolis")))
+
+;; ============
+;; ePub Reader
+;; ============
+(use-package ereader)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (ereader company-lsp wttrin emmet-mode web-mode yasnippet markdown-mode haml-mode yaml-mode company git-gutter magit counsel-projectile which-key visual-regexp use-package spaceline smex smartparens projectile neotree move-text leuven-theme flx expand-region exec-path-from-shell counsel avy all-the-icons))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
